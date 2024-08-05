29b1b04158738962bfcaa52786e7038f4bc6d070
Rhaíssa Lima <rhaissasbgl@gmail.com>
2024-08-05 12:26:46 -0300

    primeiro commit

--- src/components/Contador/Contador.tsx
+++ src/components/Contador/Contador.tsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import './Contador.css'
 
 function Contador() {
+
+  const [valor, setValor] = useState<number>(0);
+
+  function somarMaisUm() {
+    setValor(valor + 1);
+  }
+
+  console.log(valor);
+
   return (
     <div className="container">
-        <p>O valor é: valor</p>
-        <button>Adicionar +1</button>
+        <p>O valor é: {valor}</p>
+        <button onClick={somarMaisUm}>Adicionar +1</button>
     </div>
   )
 }
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from 'react'
 import './Task.css'
 
 function Task() {
+  
+  // variáveis de estado
+  const [finalizada, setFinalizada] = useState<boolean>(false);
+  const [tarefa, setTarefa] = useState<string>("tarefa pendente");
+  
+  // função de efeito colateral
+  useEffect(() => {
+    if (finalizada === true) {
+      setTarefa("tarefa concluída");
+    }
+  }, [finalizada]);
+
   return (
     <div className="container">
         <h1>Tarefa</h1>
-        <h3>Status da Tarefa</h3>
-        <button>Conclua a Tarefa</button>
+        <h3>Status da Tarefa: {tarefa}</h3>
+        <button onClick={() => setFinalizada(true)}>Conclua a Tarefa</button>
     </div>
   )
 }
